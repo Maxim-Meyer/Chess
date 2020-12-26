b3a40dc85210c02601a67b2cd420b0866274aa0c
Maxim Meyer <meyer.maxim@t-online.de>
2020-12-26 18:32:13 +0100

    Initial Commit

---
+++ macos2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macos2
+//
+//  Created by Maxim Meyer on 26.12.20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macos2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macos2
+//
+//  Created by Maxim Meyer on 26.12.20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
